c44545b0933425e3e0905865871ff23a5ebb7530
lfist9462-ux <lfist9462@gmail.com>
2026-07-20 19:50:43 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const botao = document.getElementById("start");
+
+    if (botao) {
+        botao.addEventListener("click", () => {
+            alert("O Quiz começará em breve! 💍❤️");
+        });
+    }
+});
